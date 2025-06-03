e6dad1e5c4c163dc53a5d9caff390ca1c3490e8d
Jerome Vonk <lelovv@gmail.com>
2025-06-03 18:55:59 -0300

    add round 11

--- backend-frontend/src/components/Matches.js
+++ backend-frontend/src/components/Matches.js
@@ -13,7 +13,7 @@ export default function Matches(props) {
   const { currentRound, matches } = props;
 
   const [round, setRound] = React.useState(currentRound);
-  const [page, setPage] = React.useState(3);
+  const [page, setPage] = React.useState(1);
 
   // Context
   const context = React.useContext(AppContext);
